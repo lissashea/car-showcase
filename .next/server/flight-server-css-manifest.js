171a9a23f0891a22b0dc3274fa3d44190ab5158a
“Lissa <lwarshaw674@gmail.com>
2023-09-08 18:51:53 -0400

    updated settings.json

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,14 +1,4 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "/Users/lissawarshaw/Desktop/repos/projects/car-showcase/app/layout.tsx": [
-      "/Users/lissawarshaw/Desktop/repos/projects/car-showcase/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-      "/Users/lissawarshaw/Desktop/repos/projects/car-showcase/app/globals.css"
-    ]
-  },
-  "cssModules": {
-    "/Users/lissawarshaw/Desktop/repos/projects/car-showcase/app/page": [
-      "/Users/lissawarshaw/Desktop/repos/projects/car-showcase/app/globals.css",
-      "/Users/lissawarshaw/Desktop/repos/projects/car-showcase/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
-    ]
-  }
+  "cssImports": {},
+  "cssModules": {}
 }
--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,12 @@
 
 import CustomButton from "./CustomButton";
 import Hero from "./Hero";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
 
 export {
   Hero,
   CustomButton,
+  Footer,
+  NavBar,
 };
